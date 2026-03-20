d7f1b9fadfdce5591f41a6f3245dce49e78d8d08
ErickBanhos99 <erick.bcastro99@gmail.com>
2026-03-20 11:32:04 -0300

    Aula 02 variáveis e operadores, 2 desafios de 3 concluídos.

---
+++ Desafios.py
@@ -0,0 +1,16 @@
+#DESAFIO 1
+
+nome = input("Olá, digite seu nome:")
+
+print (f"Seja bem-vindo, {nome}!")
+
+#DESAFIO 2
+
+dia_nascimento = input ("Agora, digite o dia do seu nascimento:")
+mes_nascimento = input ("Agora, digite o mes do seu nascimento:")
+ano_nascimento = input ("Agora, digite o ano do seu nascimento:")
+
+print(f"Sua data de nascimento é {dia_nascimento}/{mes_nascimento}/{ano_nascimento}")
+
+#DESAFIO 3
+
---
+++ aula02/aula02a.py
@@ -0,0 +1,29 @@
+print ("ola mundo")
+
+print (7+4)
+print ("7+4")
+print ("7" + "4") # Concatenação de string
+
+# Comentário de 1 linha
+''' 
+Comentários de 
+múltiplas 
+linhas
+'''
+
+# Variáveis
+nome = "Erick" # str
+idade = 20 # int
+peso = 74.9 # float
+
+print (nome, idade, peso)
+print (f"Fala {nome}!!!")
+
+# INPUT - SIMULAÇÂO DE UM FORMS NO CMD
+nome = input("Digite o seu nome:")
+idade = int (input("Digite sua idade:"))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(idade + 1)
